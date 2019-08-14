adaa5733cd08f9ede13d6eae4fbbefa0b82cb5cc
emmakris <makris.emmanuel@gmail.com>
2019-08-14 09:38:00 +0300

    Routes added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
-import { Button } from "reactbulma";
 import "./App.css";
+import NavbarMenu from "./Navigation";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Button primary>Primary Test</Button>
-      </header>
+      <Router>
+        <NavbarMenu />
+      </Router>
     </div>
   );
 }
